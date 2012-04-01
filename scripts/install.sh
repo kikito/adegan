d4508cab59919bef1b03d8a1f972eef815e83639
Enrique García Cota <kikito@gmail.com>
2012-04-01 21:08:17 +0200

    modified install.sh so it includes our file instead of symlinking it

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,16 @@
-rm -f ~/.vimrc
-ln -s ~/.vim/vimrc ~/.vimrc
+
+
+# add the line that includes .vim/vimrc (this lib's version) from ~/.vimrc (the default vim file)
+
+LINE="source ~/.vim/vimrc"
+FILE=$HOME/.vimrc
+
+touch "$FILE"
+if ! grep -Fxq "$LINE" "$FILE"
+then
+  echo "$LINE" >> "$FILE"
+fi
+
+
+
+
