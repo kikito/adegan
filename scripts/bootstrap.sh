9cd402d51d4382a0b8e9007fab555c28045097bb
Enrique García Cota <kikito@gmail.com>
2012-04-09 10:08:53 +0200

    bootstrap.sh now handles vimrc files a bit differently

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,12 +4,21 @@ function die()
     exit 1
 }
 
-# Add .back to any existing Vim file in the home directory
+# Add .pre-adegan to any existing Vim file in the home directory
 PREV=$HOME/.vim
 if [[ ( -e $PREV ) || ( -h $PREV ) ]]; then
-  echo "${PREV} has been renamed to ${PREV}.bak"
-  mv "${PREV}" "${PREV}.bak" || die "Could not move ${PREV} to ${PREV}.bak"
+  rm -rf "${PREV}.pre-adegan"
+  mv "${PREV}" "${PREV}.pre-adegan" || die "Could not move ${PREV} to ${PREV}.pre-adegan"
+  echo "${PREV} has been renamed to ${PREV}.pre-adegan"
 fi
+
+# move .vimrc to .vimrc.pre-adegan if it's a symlink
+PREV=$HOME/.vimrc
+if [ -L "$PREV" ]; then
+  rm -rf "${PREV}.pre-adegan"
+  mv "${PREV}" "${PREV}.pre-adegan" || die "Could not move ${PREV} to ${PREV}.pre-adegan"
+  echo "${PREV} has been renamed to ${PREV}.pre-adegan"
+end
 
 # Clone adegan into .vim
 git clone https://github.com/kikito/adegan.git $HOME/.vim \
